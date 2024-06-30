c42bb6a4506ddc43b417d5b82ff0a2c01393f066
oleg-usatyi <oleg_usatyi@ukr.net>
2024-06-30 14:26:14 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,30 +1,22 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+const loginForm = document.querySelector('.login-form');
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const emailInput = loginForm.elements['email'];
+  const passwordInput = loginForm.elements['password'];
+
+  if (!emailInput.value.trim() || !passwordInput.value.trim()) {
+    alert('All form fields must be filled in');
+  } else {
+    const formData = {
+      email: emailInput.value.trim(),
+      password: passwordInput.value.trim(),
+    };
+
+    console.log(formData);
+
+    loginForm.reset();
+  }
+});
+
